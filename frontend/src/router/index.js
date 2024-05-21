ee54cce98d8513c8d8b6c3469b94b6d1c8511dea
Hua Trung Kien <huatrungkien126@gmail.com>
2024-05-21 23:48:43 +0700

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,26 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
+export const constantRoutes = [
+  {
+    path: '/404',
+    component: () => import('@/views/404View.vue'),
+    hidden: true
+  },
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('@/views/AboutView.vue')
+  },
+  { path: '*', redirect: '/404', hidden: true }
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/404',
-      component: () => import('@/views/404View.vue'),
-      hidden: true
-    },
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('@/views/AboutView.vue')
-    },
-    { path: '*', redirect: '/404', hidden: true }
-  ]
+  history: createWebHistory(),
+  routes: constantRoutes
 })
+
+export function resetRouter() {
+  const newRouter = createRouter({
+    history: createWebHistory(),
+    routes: constantRoutes
+  })
+  router.matcher = newRouter.matcher
+}
 
 export default router
