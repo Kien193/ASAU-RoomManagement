f514ba231acaa5b67f26ee126e8335e3702d53a0
Kien <huatrungkien126@gmail.com>
2024-05-26 10:52:57 +0700

    remove

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,14 @@
 <template>
 	<div class="app">
-		<!-- Sidebar -->
 		<Sidebar />
-
-		<!-- Header -->
-		<Header />
-
-		<!-- Content -->
 		<router-view />
-
-		<!-- Footer -->
-		<Footer />
 	</div>
 </template>
 
 <script setup>
-import Header from '@/components/Headers/DashboardHeader.vue'
+
 import Sidebar from '@/components/Sidebars/DashboardSidebar.vue'
-import Footer  from '@/components/Footers/DashboardFooter.vue'
+
 </script>
 
 <style lang="scss">
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,27 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+
+// import AppLayout from '@/layouts/AppLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
+      path: '/:pathMatch(.*)*',
+      redirect: '/admin/home',
+    },
+    {
+      name: 'admin',
+      path: '/admin',
+      //component: AppLayout,
+      redirect: { name: 'Home' },
+      children: [
+        {
+          path: 'home',
+          name: 'Home',
+          component: () => import('@/views/HomeView.vue'),
+        },
+        {
+          path: 'about',
+          name: 'About',
+          component: () => import('@/views/AboutView.vue'),
+        },
+      ]
+    },
+    {
+      name: '404',
       path: '/404',
       component: () => import('@/views/404View.vue'),
-      hidden: true
-    },
-    {
-      path: '/',
-      name: 'Home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      component: () => import('@/views/AboutView.vue')
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      redirect: '/404',
-      hidden: true
     }
   ],
 })
